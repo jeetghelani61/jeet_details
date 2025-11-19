717c6fdbf4b55fe45461f190b3b82bd090127595
jeetghelani61 <jeetghelani61@gmail.com>
2025-11-19 11:07:13 +0530

    Update links to include 'jeet/' prefix

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 echo "<h2> Jeet API</h2>";
 echo "<ul>";
-echo "<li><a href='connect.php'>Connection</a></li>";
-echo "<li><a href='signup.php'>Signup</a></li>";
-echo "<li><a href='signin.php'>Signin</a></li>";
-echo "<li><a href='insert.php'>Insert Product</a></li>";
-echo "<li><a href='delete.php'>Delete Products</a></li>";
-echo "<li><a href='update.php'>Update Products</a></li>";
-echo "<li><a href='view.php'>View Products</a></li>";
+echo "<li><a href='jeet/connect.php'>Connection</a></li>";
+echo "<li><a href='jeet/signup.php'>Signup</a></li>";
+echo "<li><a href='jeet/signin.php'>Signin</a></li>";
+echo "<li><a href='jeet/insert.php'>Insert Product</a></li>";
+echo "<li><a href='jeet/delete.php'>Delete Products</a></li>";
+echo "<li><a href='jeet/update.php'>Update Products</a></li>";
+echo "<li><a href='jeet/view.php'>View Products</a></li>";
 echo "</ul>";
 ?>
+
