1e7d9cb1a91d5f67ead9e8ab9a0cd78ab6965dd4
jeetghelani61 <jeetghelani61@gmail.com>
2025-12-01 11:48:09 +0530

    Update view.php

--- view.php
+++ view.php
@@ -10,25 +10,10 @@ while($data = mysqli_fetch_assoc($res))
 {
     $row = array();
     $row["id"]    = $data["id"];
-    $row["price"] = $data["p_price"];
-    $row["des"]   = $data["p_des"];
-    $row["name"]  = $data["p_name"];  
-    <?php
-
-include('connect.php');
-
-$sql = "SELECT * FROM jeet_products";
-$response = array();
-$res = mysqli_query($con, $sql);
-
-while($data = mysqli_fetch_assoc($res))
-{
-    $row = array();
-    $row["id"]    = $data["id"];
-    $row["price"] = $data["p_price"];
-    $row["des"]   = $data["p_des"];
-    $row["name"]  = $data["p_name"];  
-    $row["img"]  = $data["p_img"];  
+    $row["p_price"] = $data["p_price"];
+    $row["p_des"]   = $data["p_des"];
+    $row["p_name"]  = $data["p_name"];  
+    $row["p_img"]  = $data["p_img"];  
     $response[] = $row;
 }
 
@@ -36,10 +21,6 @@ echo json_encode($response);
 
 ?>
 
-    $response[] = $row;
-}
 
-echo json_encode($response);
 
-?>
 
