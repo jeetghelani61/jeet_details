8ff5415e966f5fa3354f249908e303cc9b94e1d5
jeetghelani61 <jeetghelani61@gmail.com>
2025-12-01 11:33:37 +0530

    Add image field to product response in view.php

--- view.php
+++ view.php
@@ -13,9 +13,33 @@ while($data = mysqli_fetch_assoc($res))
     $row["price"] = $data["p_price"];
     $row["des"]   = $data["p_des"];
     $row["name"]  = $data["p_name"];  
+    <?php
+
+include('connect.php');
+
+$sql = "SELECT * FROM jeet_products";
+$response = array();
+$res = mysqli_query($con, $sql);
+
+while($data = mysqli_fetch_assoc($res))
+{
+    $row = array();
+    $row["id"]    = $data["id"];
+    $row["price"] = $data["p_price"];
+    $row["des"]   = $data["p_des"];
+    $row["name"]  = $data["p_name"];  
+    $row["img"]  = $data["p_img"];  
     $response[] = $row;
 }
 
 echo json_encode($response);
 
 ?>
+
+    $response[] = $row;
+}
+
+echo json_encode($response);
+
+?>
+
